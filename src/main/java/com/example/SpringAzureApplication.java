76c7f1bae9f241f99c758d6aa3fc42f5d050c015
aman9saw <sawaman9@gmail.com>
2022-05-18 12:43:27 +0530

    changing message

--- src/main/java/com/example/SpringAzureApplication.java
+++ src/main/java/com/example/SpringAzureApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureApplication {
 	
 	@GetMapping("/message")
 	public String getMessage() {
-		return "Hello World";
+		return "Hello Worlds";
 	}
 
 	public static void main(String[] args) {
